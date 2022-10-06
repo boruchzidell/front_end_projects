31465c28d9425b8e4814f9118ff3ce9c4f638fb4
Boruch Zidell <47635324+boruchzidell@users.noreply.github.com>
2022-10-06 09:02:33 -0400

    nest todo_web_storageapp inside front_end_projects

---
+++ todo_web_storage/README.md
@@ -0,0 +1,7 @@
+# Todo List
+
+A todo list in a SPA (Single Page App).
+
+Uses web storage to persist data locally.
+
+[Live](https://boruchzidell.github.io/todolist_web_storage)
---
+++ todo_web_storage/todoList.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let getElement = (selector) => document.querySelector(selector);
+
+  let item = getElement('#item');
+  let list = getElement('#list');
+  let button = getElement('button');
+
+  function addLi(content) {
+    let li = document.createElement('li');
+    li.textContent = content;
+    list.appendChild(li);
+  }
+
+  function loadData() {
+    itemsArray.forEach(addLi);
+  }
+
+  // Initialize array
+  let itemsArray = localStorage.getItem('items')
+    ? JSON.parse(localStorage.getItem('items'))
+    : [];
+
+  loadData();
+
+  // Add items
+  document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    let todoItem = item.value;
+    itemsArray.push(todoItem);
+    localStorage.setItem('items', JSON.stringify(itemsArray));
+
+    while (list.firstChild) {
+      list.firstChild.remove();
+    }
+
+    loadData();
+
+    this.reset();  // clear form
+  });
+
+  // Obliterate entire list
+  button.addEventListener('click', function (e) {
+    localStorage.clear();
+
+    while (list.firstChild) {
+      list.firstChild.remove();
+    }
+  });
+});
